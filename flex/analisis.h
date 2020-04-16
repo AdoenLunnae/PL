e4f1bfbc36278c3ce94c6d7aafea1a62556822eb
Adoen Lunnae <unshekdelmonton@gmail.com>
2020-04-16 18:21:15 +0200

    Exercise 3 => Programming language components

---
+++ flex/analisis.h
@@ -0,0 +1,56 @@
+#define SI 257
+#define ENTONCES 258
+#define SINO 259
+#define FINSI 260
+
+#define MIENTRAS 261
+#define HACER 262
+#define FINMIENTRAS 263
+
+#define REPETIR 264
+#define HASTA_QUE 265
+
+#define PARA 266
+#define DESDE 267
+#define HASTA 268
+#define PASO 269
+#define FINPARA 270
+
+#define IDENT 271
+#define NUMERO 272
+
+#define CADENA 273
+
+#define ASIGNA 274
+
+#define SUMA 275
+#define RESTA 276
+#define MULT 277
+#define DIV 278
+#define MOD 279
+#define POW 280
+
+#define CONCAT 281
+
+#define MENOR_QUE 282
+#define MENOR_IGUAL 283
+#define MAYOR_QUE 284
+#define MAYOR_IGUAL 285
+#define IGUAL 286
+#define DISTINTO 287
+
+#define DISYUNCION 288
+#define CONJUNCION 289
+#define NEGACION 290
+
+#define FIN_SENTENCIA 291
+#define PAR_IZQ 292
+#define PAR_DER 293
+
+#define INVALID 294
+
+#define INICIO 295
+#define FIN 296
+
+#define ESCRIBIR 297
+#define LEER 298
